9a690271882cbce767c88c820170ecadff47c568
qubick <qubick.kim@gmail.com>
2018-02-05 20:16:12 -0700

    serial port connected but commands are queued and sent when it is connected by other host (repetier, cura)

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-var express = require("express")
+var express = require("express");
 var app = express();
 var fs = require("fs");
 var http = require("http");
-
 var reqIdx = 0;
+
+// serial port is not binding
+var SerialPort = require("serialport");
+// var SerialPort = serialport.SerialPort; 
+var port;
 
 //server main page1 - video stream based
 app.get("/", (req, res) => {
@@ -11,7 +15,18 @@ app.get("/", (req, res) => {
 });
 
 app.listen(5555, () => {
-	console.log("example app listening on port 5555")
+	console.log("HFI controller app listening on port 5555");
+	port = new SerialPort('/dev/cu.usbmodem1411', {
+		baudRate: 57600
+	});
+	
+	if(port){
+		console.log("serial port opened to the printer")
+		port.write('G0 X10 F1800')
+	}
+	else {
+		console.log("failed to open port")
+	}
 });
 
 app.post("/user/add", function(req, res){
@@ -22,5 +37,3 @@ app.get(/^(.+)$/, function(req, res){
 	console.log('static file requre: ' + req.params);
 	res.sendFile(__dirname + req.params[0]);
 });
-
-// console.log("objects", objects.length)
