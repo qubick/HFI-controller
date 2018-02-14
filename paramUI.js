802d22bb077468620d9a5022bc0ba71e16333b75
qubick <qubick.kim@gmail.com>
2018-02-13 20:26:05 -0700

    start print btn send POST msg to server to send command to 3D printer

--- paramUI.js
+++ paramUI.js
@@ -4,45 +4,57 @@ var lrScaleUIAdded = false, topScaleUIAdded = false, modelLoaded = false;
 var leftBoxUI, rightBoxUI, topBoxUI;
 
 
-var settings = {
-}
+// var settings = {
+//   layer = 0.0;
+// }
+
 var panel = new dat.GUI();
 
-var params = {
-  loadFile: function(){
+var Params = function(){
+  this.loadFile = function(){
     
-    document.getElementById('myInput').click(); //bind file opener
+    document.getElementById('loadFileInput').click(); //bind file opener
+  };
 
-  },
+  this.print = function() {
+    // port = new SerialPort('/dev/usbmodem1421'); //immediately opens a port
+    // 
+    // port.write('main screen turn on', function(err){
+    //   if (err){
+    //     return console.log('Error on write:', err.message);
+    //   }
+    //   console.log('message written');
+    // });
+    
+    //start to send a message
+    // console.log("clicked button: ", document.getElementById('printStartInput'))
+    document.getElementById('printStartInput').click();
+  };
 
-  connect:function() {
-    port = new SerialPort('/dev/usbmodem1421'); //immediately opens a port
-
-    port.write('main screen turn on', function(err){
-      if (err){
-        return console.log('Error on write:', err.message);
-      }
-      console.log('message written');
-    });
-  },
-
-  export: function(){
+  this.export = function(){
     console.log("export stl")
-  }
+  };
+  this.layer = 0.0
 }
 // var modelUI = panel.addFolder( 'Model Scale' );
 
 
 function createPanel(){
   //file upload
+  var params = new Params();
   panel.add(params, 'loadFile').name('Load 3D Model');
-  panel.add(params, 'connect').name('Connect to Printer');
-  panel.add(params, 'export').name('Export to STL');
-
-  // modelUI.open();
+  panel.add(params, 'print').name('Start printing');
+  // panel.add(params, 'export').name('Export to STL');
+  panel.add(params, 'layer', 0, 300).name('See layers').listen(()=>{
+  //   for(let i=0; i<layerNumber; i++){
+  //     slicingGeometry.vertices.push(new THREE.Vector3(mvmt[i].x, mvmt[i].y-100, mvmt[i].z)); //since our grid is at (0, -100)
+  //   }
+  //   var line3D = new THREE.Line(slicingGeometry, lineMaterial);
+  //   scene.add(line3D);
+  // 
+  }); //get the layer number from gcode reader
 }
 
-function removePanel(gearType){
-  topBoxUI.close();
+function removePanel(){
   delete topBoxUI;
 }
--- server.js
+++ server.js
@@ -4,15 +4,22 @@ var fs = require("fs");
 var http = require("http");
 var reqIdx = 0;
 
-// serial port is not binding
 var SerialPort = require("serialport");
-// var SerialPort = serialport.SerialPort; 
 var port;
 
-//server main page1 - video stream based
+//server main page
 app.get("/", (req, res) => {
 	res.sendfile('index.html')
 });
+
+app.post("/", function(req, res){
+	console.log("POST message recieved from the app")
+	console.log(req.body);
+	// res.send(200); //no response w/ ('ok') message
+	res.sendfile('index.html'); //refresh screen
+	
+	sendCommand();
+})
 
 app.listen(5555, () => {
 	console.log("HFI controller app listening on port 5555");
@@ -22,9 +29,9 @@ app.listen(5555, () => {
 	
 	if(port){
 		console.log("serial port opened to the printer");
-		// port.write('M109 S250.000000\n');
+		// port.write('M109 S250.000000\n'); //set temperature and wait until reach for the next command
 		port.write('G0 X0 Y0 Z10 F1800\n'); //home all axis
-		port.write('G0 X30 Y100 F1800\n');
+		port.write('G0 X30 Y50 F1800\n'); //text move
 		port.write('G0 Y30\n'); //step by step
 	}
 	else {
@@ -40,3 +47,7 @@ app.get(/^(.+)$/, function(req, res){
 	console.log('static file requre: ' + req.params);
 	res.sendFile(__dirname + req.params[0]);
 });
+
+function sendCommand(){
+	console.log("start sending commands...");
+}
