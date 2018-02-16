9d133821f29cdc78aa0d9a8269088c027ae649b9
qubick <qubick.kim@gmail.com>
2018-02-16 16:41:42 -0700

    Revert "inserted object is captured and subtracted from the previous scene"
    This reverts commit b2c1c2153910134c5a1df8f90173eb5793f2504a.

--- cam.js
+++ cam.js
@@ -19,14 +19,13 @@ function take_snapshot() {
   
   if(pausedPrint){
     document.getElementById('snapshotBtn').value = "Resume"
+    
     imgTag = "firstImg";
     divTag = "results1";
-    
-    //if in the middle of printing, completes the entire layer
-    
   }
   else {
     document.getElementById('snapshotBtn').value = "Pause Print"
+    
     imgTag = "secondImg";
     divTag = "results2";
     
@@ -75,21 +74,10 @@ function doImageProcessing(){
       cv.cvtColor(imgSecnd, imgSecnd, cv.COLOR_RGBA2RGB, 0);
       let bgdModel = new cv.Mat();
       let fgdModel = new cv.Mat();
-      let rect = new cv.Rect(50,10,200,200); //set to printing base size shown in the cam
+      let rect = new cv.Rect(50,50,150,150);
       cv.grabCut(imgSecnd, mask, rect, bgdModel, fgdModel, 1, cv.GC_INIT_WITH_RECT);
       
-      //draw foreground for img1
-      for(let i=0; i<imgFirst.rows; i++){
-        for(let j=0; j<imgFirst.cols; j++){
-          if(mask.ucharPtr(i,j)[0] === 0 || mask.ucharPtr(i,j)[0] === 2){
-            imgFirst.ucharPtr(i,j)[0] = 255;
-            imgFirst.ucharPtr(i,j)[1] = 255;
-            imgFirst.ucharPtr(i,j)[2] = 255;
-          }
-        }
-      }
-      
-      //draw foreground for img2
+      //draw foreground
       for(let i=0; i<imgSecnd.rows; i++){
         for(let j=0; j<imgSecnd.cols; j++){
           if(mask.ucharPtr(i,j)[0] === 0 || mask.ucharPtr(i,j)[0] === 2){
@@ -101,39 +89,63 @@ function doImageProcessing(){
       }
       
       //draw grab Rect
-      // let color   = new cv.Scalar(0,0,255);
-      // let point1  = new cv.Point(rect.x, rect.y);
-      // let point2  = new cv.Point(rect.x + rect.width, rect.y + rect.height);
-      // cv.rectangle(imgFirst, point1, point2, color);
-      // cv.rectangle(imgSecnd, point1, point2, color);
-      cv.imshow('foreground1', imgFirst);
-      cv.imshow('foreground2', imgSecnd);
+      let color = new cv.Scalar(0,0,255);
+      let point1 = new cv.Point(rect.x, rect.y);
+      let point2 = new cv.Point(rect.x + rect.width, rect.y + rect.height);
+      cv.rectangle(imgSecnd, point1, point2, color);
+      cv.imshow('subtResult1', imgSecnd);
       
-      //thresholding to find difference      
-      cv.cvtColor(imgFirst, imgFirst, cv.COLOR_RGBA2GRAY, 0);
+      
+      // find contour for extracted forground image
+      let dst2 = cv.Mat.zeros(imgSecnd.cols, imgSecnd.rows, cv.CV_8UC3);
       cv.cvtColor(imgSecnd, imgSecnd, cv.COLOR_RGBA2GRAY, 0);
-      cv.threshold(imgFirst, imgFirst, 100, 200, cv.THRESH_BINARY);
-      cv.threshold(imgSecnd, imgSecnd, 100, 200, cv.THRESH_BINARY);
-  
-      
-      cv.subtract(imgSecnd, imgFirst, dst, mask, dtype);
-      // cv.imshow('substResult1', dst);
-      
-      // find contour for extracted forground images
-      let newDst = cv.Mat.zeros(dst.cols, dst.rows, cv.CV_8UC3);
+      cv.threshold(imgSecnd, imgSecnd, 120, 200, cv.THRESH_BINARY);
       let contours = new cv.MatVector();
       let hierarchy = new cv.Mat();
       // 
-      cv.findContours(dst, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
       for (let i = 0; i < contours.size(); ++i) {
         //random colors
         let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
                               Math.round(Math.random() * 255));
-        cv.drawContours(newDst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+        cv.drawContours(dst2, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
       }
       
-      cv.imshow('substResult2', newDst);
-      dst.delete();
-      newDst.delete();
+      //params to findcontours
+      // let dst1 = cv.Mat.zeros(imgFirst.cols, imgFirst.rows, cv.CV_8UC3);
+      // let dst2 = cv.Mat.zeros(imgSecnd.cols, imgSecnd.rows, cv.CV_8UC3);
+      // 
+      // cv.cvtColor(imgFirst, imgFirst, cv.COLOR_RGBA2GRAY, 0);
+      // cv.cvtColor(imgSecnd, imgSecnd, cv.COLOR_RGBA2GRAY, 0);
+      // 
+      // cv.threshold(imgFirst, imgFirst, 120, 200, cv.THRESH_BINARY);
+      // cv.threshold(imgSecnd, imgSecnd, 120, 200, cv.THRESH_BINARY);
+      // let contours = new cv.MatVector();
+      // let hierarchy = new cv.Mat();
+      // 
+      // cv.findContours(imgFirst, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+      // cv.findContours(imgSecnd, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+      // 
+      // //find contours for both images
+      // for (let i = 0; i < contours.size(); ++i) {
+      //   //random colors
+      //   let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
+      //                         Math.round(Math.random() * 255));
+      //   cv.drawContours(dst1, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+      // }
+      // 
+      // 
+      // for (let i = 0; i < contours.size(); ++i) {
+      //   //random colors
+      //   let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
+      //                         Math.round(Math.random() * 255));
+      //   cv.drawContours(dst2, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+      // }
+      
+      
+      // cv.subtract(dst2, dst1, dst, mask, dtype);
+      
+      // cv.threshold(dst,dst,15,255,cv.THRESH_BINARY);
+      cv.imshow('subtResult2', dst2);
+      // dst.delete();
     }
 }
