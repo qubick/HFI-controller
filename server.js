a34fa7124026ae01e32415261fcb2c6fbff75632
qubick <qubick.kim@gmail.com>
2018-02-22 16:19:50 -0700

    now server sends gcode commands to printer need to control it line by line using internal queue

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+//server console message color scheme: Magenta
+
 var express = require("express");
 var app = express();
 var fs = require("fs");
 var http = require("http");
-var exec = require('child_process').execSync, child;
+var exec = require('child_process').execSync, child; //ensure asynchronous
 var colors = require('colors');
 
 //for serial connection with the printer
 var SerialPort = require("serialport");
-var port;
+var port = new SerialPort('/dev/cu.usbmodem1411', {
+	baudRate: 57600
+});
 
 //for message channel with the client
 var pendingResponses = {};
@@ -27,22 +31,22 @@ app.get("/", (req, res) => {
 });
 
 app.listen(5555, () => {
-	console.log("HFI controller app listening on port 5555");
+	console.log('[Server]'.magenta, ' HFI controller app listening on port 5555'.white);
 
 	//create connection with the 3D printer
-	port = new SerialPort('/dev/cu.usbmodem1411', {
-		baudRate: 57600
-	});
+	// port = new SerialPort('/dev/cu.usbmodem1411', {
+	// 	baudRate: 57600
+	// });
 
 	//create connection with the leapMotion
 	leapMotion.leapMotion();
 
 	if(port){
-		console.log('\nSerial port to the 3D printer opened'.magenta);
+		console.log('[Server]'.magenta, 'Serial port to the 3D printer opened'.white);
 		// port.write('M109 S250.000000\n'); //set temperature and wait until reach for the next command
 		port.write('G0 F3600 X30 Y50 \n'); //test move
 		port.write('G0 Y30\n'); //step by step
-		port.write('G0 F1800 X0 Y0 Z0 \n'); //home all axis
+		port.write('G28 F1800 X Y Z \n'); //home all axis
 
 		// while (!gcodeQueue.isEmpty){
 		// 	var line = gcodeQueue.pop();
@@ -51,7 +55,7 @@ app.listen(5555, () => {
 
 	}
 	else {
-		console.log("failed to open port")
+		console.log('[Server]'.magenta, "failed to open port".red)
 	}
 });
 
@@ -116,7 +120,7 @@ http.createServer((req, res) => {
 				  if(err) return console.log(err);
 
 					let cmd1 = 'openjscad output/output.jscad';
-					let cmd2 = './CuraEngine/build/CuraEngine slice -j ./CuraEngine/resources/definitions/printrbot_play.def.json -e0 -s infill_line_distance=1 -l "output/output.stl" -o "output/test.gcode"';
+					let cmd2 = './CuraEngine/build/CuraEngine slice -j ./CuraEngine/resources/definitions/printrbot_play.def.json -e0 -l "output/output.stl" -o "output/test.gcode"';
 					runCommandline(cmd1, cmd2);
 
 				});
@@ -151,7 +155,7 @@ http.createServer((req, res) => {
     res.setHeader("Content-Type", "text/html");
   }
 }).listen(5000, () => {
-	console.log("The HTTP message channel is listening on 5000".magenta);
+	console.log('[Server]'.magenta, 'The HTTP message channel is listening on 5000'.white);
 });
 
 //listening msg from app
@@ -173,27 +177,34 @@ function sendCommand(){
 
 function runCommandline(cmd1, cmd2){
 
-	child = exec(cmd1, (err, stdout, stderr)=>{
+	console.log("runnding two commands..")
 
+	child = exec(cmd1, (err, stdout, stderr)=>{
 		console.log('running openjscad script, stdout: '.magenta + stdout);
 		if(err) console.log("exec error from running openjscad script: ".blue, err);
+		if(stderr) console.log('\n[Server]'.magenta, strerr.red);
 	});
 
+	//once the 1st cmd (run openjscad) done
 	child = exec(cmd2, (err, stdout, stderr)=>{
 		// console.log('running slicer, stdout: '.blue + stdout);
-
 		if(err) console.log('exec error from running slicer: '.blue, err);
+		if(stderr) console.log('\n[Server]'.magenta, strerr.red);
+	});
 
-		fs.readFile("output/test.gcode", "utf8", (err, data) => {
-			if(err) throw err;
+	//once the 2nd cmd (run cura engine)
+	fs.readFile("output/test.gcode", "utf8", (err, data) => {
+		if(err) throw err;
 
-			console.log(data.blue);
-			var gcodes = data.split('\n');
-			for(var a=0; a<100; a++){
-				console.log("writing gcode: ", gcodes[a]);
-				port.write(gcodes[a]+'\n'); //send gcode line by line
-			}
+		// console.log(data.blue);
+		var gcodes = data.split('\n');
+		gcodes.forEach((gcodeline) =>{
+			console.log("writing gcode: ".blue, gcodeline);
+			port.write(gcodeline + '\n'); //send gcode line by line
 		});
-
+		// for(var a=0; a<100; a++){
+		// 	console.log("writing gcode: ".blue, gcodes[a]);
+		// 	port.write(gcodes[a]+'\n'); //send gcode line by line
+		// }
 	});
 }
